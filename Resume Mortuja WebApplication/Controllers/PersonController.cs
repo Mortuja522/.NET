81fb7d935ee90ec9863eb280e683937689223f06
Mortuja Morshed <75001023+Mortuja522@users.noreply.github.com>
2021-09-30 13:34:01 +0600

    First Commit

---
+++ Resume Mortuja WebApplication/Controllers/PersonController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Resume_Mortuja_WebApplication.Controllers
+{
+    public class PersonController : Controller
+    {
+        // GET: Person
+        public ActionResult Objectives()
+        {
+            return View();
+        }
+
+        public ActionResult Education()
+        {
+            return View();
+        }
+
+
+        public ActionResult Personalinfo()
+        {
+            return View();
+        }
+
+        public ActionResult Project()
+        {
+            return View();
+        }
+
+        public ActionResult Reference()
+        {
+            return View();
+        }
+    }
+}
---
+++ Resume Mortuja WebApplication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Resume_Mortuja_WebApplication.MvcApplication" Language="C#" %>
